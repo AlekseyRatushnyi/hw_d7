cb6f9bddf785803b5dc650950ffb8b8c4d4fdbb4
Aleksey <RatushnyiAA1982@gmail.com>
2023-03-28 22:56:12 +0300

    Подписка+рассылка+еженедельная рассылка

--- NewsPaper/sign/models.py
+++ NewsPaper/sign/models.py
@@ -1,8 +1,11 @@
+from django.conf import settings
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 from allauth.account.forms import SignupForm
 from django.contrib.auth.models import Group
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 
 
 class BaseRegisterForm(UserCreationForm):
@@ -20,10 +23,27 @@ class BaseRegisterForm(UserCreationForm):
                   "password2", )
 
 
+
+def send_greeting(user_email):
+    msg = EmailMultiAlternatives(
+        subject="Приветствуем",
+        body='Вы зарегестировались на новостном портале!',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=user_email
+        )
+
+    msg.send()
+
 class BasicSignupForm(SignupForm):
 
+
     def save(self, request):
+        user_email = []
         user = super(BasicSignupForm, self).save(request)
         basic_group = Group.objects.get(name='common')
         basic_group.user_set.add(user)
+        user_email.append(user.email)
+        send_greeting(user_email)
         return user
+
+
